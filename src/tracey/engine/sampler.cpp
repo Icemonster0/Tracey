382c8ba64012a4ed193d52d49c2dd49d338b0de7
Andras <andras.borsanyi09@gmail.com>
2024-02-09 14:03:03 +0100

    Optimize RNG usage in sampler

--- src/tracey/engine/sampler.cpp
+++ src/tracey/engine/sampler.cpp
@@ -18,41 +18,45 @@ Sampler::Sampler(glm::ivec2 frame_size) {
 void Sampler::render(glm::ivec2 frame_size, Camera *camera, Accelerator *accelerator, ShaderPack *shader_pack, uint64_t seed) {
     clear_fbuf(frame_size, glm::vec3 {0.f});
 
-    #pragma omp parallel for schedule(static) collapse(2)
-    for (int x = 0; x < frame_size.x; ++x) {
-        for (int y = 0; y < frame_size.y; ++y) {
+    // #pragma omp parallel for schedule(static) collapse(2)
+    #pragma omp parallel
+    {
+        // seed the rng
+        int thread_n = omp_get_thread_num();
+        RNG rng {seed, seed << thread_n, (seed >> 12) + 0xabc, seed * thread_n >> 31};
+        rng.jump(); // offset into the rng, because the first bits are basically independent of the seed
 
-            // seed the rng with arbitrary numbers that depend on seed, x and y
-            RNG rng {(uint64_t)x * y + seed, (uint64_t)y + x - seed, (uint64_t)x << 6 * seed, (uint64_t)y >> 9 * seed};
-            rng.jump(); // offset into the rng, because the first bits are basically independent of the seed
+        std::uniform_real_distribution<float> sample_offset_distrib {-0.5f, 0.5f};
 
-            std::uniform_real_distribution<float> sample_offset_distrib {-0.5f, 0.5f};
-
-            glm::vec2 screen_coords {
-                ((float)x + sample_offset_distrib(rng)) * 2.f / (float)frame_size.x - 1.f,
-                ((float)y + sample_offset_distrib(rng)) * 2.f / (float)frame_size.y - 1.f
-            };
-
-            Ray ray {
-                camera->get_pos(),
-                camera->calc_ray_dir_at(screen_coords),
-                TRC_CAMERA_RAY,
-                0
-            };
-
-            std::optional<Intersection> isect = accelerator->calc_intersection(ray);
-            if (isect) {
-                ShaderData shader_data {
-                    isect.value().pos,
-                    isect.value().normal,
-                    isect.value().tex_coord,
-                    isect.value().distance,
-                    ray,
-                    accelerator,
-                    shader_pack
+        #pragma omp for schedule(static) collapse(2)
+        for (int x = 0; x < frame_size.x; ++x) {
+            for (int y = 0; y < frame_size.y; ++y) {
+                glm::vec2 screen_coords {
+                    ((float)x + sample_offset_distrib(rng)) * 2.f / (float)frame_size.x - 1.f,
+                    ((float)y + sample_offset_distrib(rng)) * 2.f / (float)frame_size.y - 1.f
                 };
-                glm::vec3 color = isect.value().shader->evaluate(shader_data);
-                *fbuf.at({x, y}) = color.rgb();
+
+                Ray ray {
+                    camera->get_pos(),
+                    camera->calc_ray_dir_at(screen_coords),
+                    TRC_CAMERA_RAY,
+                    0
+                };
+
+                std::optional<Intersection> isect = accelerator->calc_intersection(ray);
+                if (isect) {
+                    ShaderData shader_data {
+                        isect.value().pos,
+                        isect.value().normal,
+                        isect.value().tex_coord,
+                        isect.value().distance,
+                        ray,
+                        accelerator,
+                        shader_pack
+                    };
+                    glm::vec3 color = isect.value().shader->evaluate(shader_data);
+                    *fbuf.at({x, y}) = color.rgb();
+                }
             }
         }
     }
